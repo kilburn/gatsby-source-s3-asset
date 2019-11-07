a0e1694f71b35d883e9ca1fa68068a0cf42dc474
Ryan Johnston <ryan@bespokeview.com>
2019-11-07 07:22:05 -0700

    Fixes loadNodeContent error (#4)
    * Fixes loadNodeContent error
    
    * compliancy fix
    
    * Fixes loadNodeContent error

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import S3AssetNode from './types/S3AssetNode'
 
+const loadNodeContent = (fileNode: object) => {
+    return fs.readFile(fileNode.absolutePath, `utf-8`)
+}
+
+exports.loadNodeContent = loadNodeContent
+
 export { S3AssetNode }
