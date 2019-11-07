b9aa32521521b2996de597782ddbea3d34156c1d
Ryan Johnston <ryanj@ryanj.org>
2019-11-07 06:30:29 -0700

    Fixes loadNodeContent error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import S3AssetNode from './types/S3AssetNode'
 
-const loadNodeContent = (fileNode: any) => {
+const loadNodeContent = (fileNode: object) => {
     return fs.readFile(fileNode.absolutePath, `utf-8`)
 }
 
