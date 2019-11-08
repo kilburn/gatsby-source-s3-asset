010489c8a4eadabed8b0b7c0bb7f8436eacb1b97
Ryan Johnston <ryanj@ryanj.org>
2019-11-08 04:18:26 -0700

    Pass tests and test with clean gatsby install.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import S3AssetNode from './types/S3AssetNode'
 
-const loadNodeContent = (fileNode: object) => {
-    return fs.readFile(fileNode.absolutePath, `utf-8`)
+const fs = require('fs')
+
+interface fileNode {
+	absolutePath?: string
+}
+
+const loadNodeContent = (fileNode: fileNode) => {
+    return fs.readFile(fileNode.absolutePath, (err) => {
+  	if (err) throw err
+    })
 }
 
 exports.loadNodeContent = loadNodeContent
